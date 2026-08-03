3551e28953a7a4f9b025e6f6112003cc42b70b9f
saikiran58347 <saikiran.23mic7048@vitapstudent.ac.in>
2026-08-03 20:39:16 +0530

    Implemented basic login functionality

--- src/main/java/com/saikiran/skillbridge/controller/AuthController.java
+++ src/main/java/com/saikiran/skillbridge/controller/AuthController.java
@@ -1,19 +1,66 @@
 package com.saikiran.skillbridge.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.saikiran.skillbridge.entity.User;
+import com.saikiran.skillbridge.repository.UserRepository;
+import com.saikiran.skillbridge.service.UserService;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
+
     @GetMapping("/register")
     public String registerPage() {
         return "register";
+    }
+
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+
+        userService.registerUser(user);
+
+        return "redirect:/login";
+    }
+
+
+    @PostMapping("/login")
+    public String loginUser(
+            @RequestParam String email,
+            @RequestParam String password) {
+
+
+        Optional<User> user = userRepository.findByEmail(email);
+
+
+        if(user.isPresent() && user.get().getPassword().equals(password)) {
+
+            return "redirect:/";
+
+        }
+
+
+        return "redirect:/login";
     }
 
 }
